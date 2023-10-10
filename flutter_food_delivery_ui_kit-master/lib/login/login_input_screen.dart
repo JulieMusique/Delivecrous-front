f814b95ef17cb7d6e7f139c1357f5601000afe91
fathounesalma <143490414+fathounesalma@users.noreply.github.com>
2023-10-10 09:20:49 +0200

    Partie user (#3)
    * Partie user : login & sign up

--- flutter_food_delivery_ui_kit-master/lib/login/login_input_screen.dart
+++ flutter_food_delivery_ui_kit-master/lib/login/login_input_screen.dart
@@ -1,9 +1,14 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:flutter_ui_food_delivery_app/utils/colors.dart';
 import 'package:flutter_ui_food_delivery_app/utils/routes.dart';
 import 'package:flutter_ui_food_delivery_app/widgets/custom_button.dart';
 import 'package:flutter_ui_food_delivery_app/widgets/custom_input.dart';
 import 'package:flutter_ui_food_delivery_app/widgets/custom_text.dart';
+
+import '../model/User.dart';
 
 class LoginInputScreen extends StatefulWidget {
   LoginInputScreen();
@@ -16,6 +21,8 @@ class _LoginInputScreenState extends State<LoginInputScreen> {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
 
+  bool isLoading = false;
+
   @override
   void initState() {
     super.initState();
@@ -24,6 +31,72 @@ class _LoginInputScreenState extends State<LoginInputScreen> {
   @override
   void dispose() {
     super.dispose();
+  }
+
+  Future<void> fetchData() async {
+    print(isLoading);
+    try {
+      final username = 'user';
+      final password = '09127daa-1d29-4749-98a8-b1a50eb5af20';
+
+      final headers = {
+        'Authorization':
+            'Basic ' + base64Encode(utf8.encode('$username:$password')),
+      };
+
+      final response = await http.get(
+        Uri.parse('http://localhost:8080/DelivCROUS/users'),
+        headers: headers,
+      );
+
+      if (response.statusCode == 200) {
+        final jsonData = json.decode(response.body);
+        print(jsonData);
+      } else {
+        // Gestion des erreurs en cas de statut de réponse autre que 200
+        print('Erreur HTTP: ${response.statusCode}');
+      }
+    } catch (e) {
+      // Gestion des erreurs en cas d'exception
+      print('Erreur lors de la requête HTTP: $e');
+    }
+  }
+
+  Future<void> loginUser() async {
+    setState(() {
+      isLoading = true;
+    });
+
+    final String email = emailController.text;
+    final String password = passwordController.text;
+    
+    try {
+      final response = await http.get(
+        Uri.parse('http://localhost:8080/DelivCROUS/users/login/$email/$password'),
+      );
+      print(response.statusCode);
+
+      setState(() {
+        isLoading = false;
+      });
+
+      if (response.statusCode == 200) {
+        final Map<String, dynamic> userData = json.decode(response.body);
+        print(userData);
+        final User user = User.fromJson(userData);
+        Navigator.pushNamed(context, Routes.home);
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('Erreur d\'authentification'),
+            backgroundColor: Colors.red,
+          ),
+        );
+      }
+    } catch (e) {
+      // Gestion des erreurs en cas d'exception
+      print('Erreur lors de la requête HTTP: $e');
+    }
   }
 
   @override
@@ -46,7 +119,8 @@ class _LoginInputScreenState extends State<LoginInputScreen> {
               padding: EdgeInsets.only(bottom: 42),
               child: AppInputText(
                 controller: passwordController,
-                hint: "Password", // Invite de champ pour le mot de passe
+                hint: "Password",
+                obscureText: true, 
               ),
             ),
             // Texte "Forgot password?" pour réinitialiser le mot de passe
@@ -63,17 +137,24 @@ class _LoginInputScreenState extends State<LoginInputScreen> {
                 ),
               ),
             ),
-            // Bouton "Login" pour se connecter
-            AppButton(
-              bgColor: vermilion,
-              borderRadius: 30,
-              fontSize: 17,
-              fontWeight: FontWeight.w600,
-              onTap: () {
-                Navigator.pushNamed(context, Routes.home); // Naviguer vers la page d'accueil
-              },
-              text: "Login", // Texte du bouton de connexion
-              textColor: athens_gray, // Couleur du texte du bouton
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: vermilion,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+              ),
+              onPressed: isLoading
+                  ? null
+                  : loginUser, // Utilisez onPressed au lieu de onTap loginUser
+              child: Text(
+                isLoading ? 'Loading...' : 'Login',
+                style: TextStyle(
+                  fontSize: 17,
+                  fontWeight: FontWeight.w600,
+                  color: athens_gray,
+                ),
+              ),
             ),
           ],
         ),
--- flutter_food_delivery_ui_kit-master/lib/login/sign_up_input_screen.dart
+++ flutter_food_delivery_ui_kit-master/lib/login/sign_up_input_screen.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_ui_food_delivery_app/login/login_input_screen.dart';
+import 'package:http/http.dart' as http;
 import 'package:flutter_ui_food_delivery_app/model/User.dart';
 import 'package:flutter_ui_food_delivery_app/login/profile/profile_screen.dart';
 import 'package:flutter_ui_food_delivery_app/utils/colors.dart';
@@ -13,8 +17,12 @@ class SignUpInputScreen extends StatefulWidget {
 }
 
 class _SignUpInputScreenState extends State<SignUpInputScreen> {
-  final TextEditingController nameController = TextEditingController();
+  final TextEditingController firstNameController = TextEditingController();
+  final TextEditingController lastNameController = TextEditingController();
   final TextEditingController emailController = TextEditingController();
+  final TextEditingController phoneController = TextEditingController();
+  final TextEditingController addressController = TextEditingController();
+  final TextEditingController loginController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
 
   @override
@@ -24,10 +32,34 @@ class _SignUpInputScreenState extends State<SignUpInputScreen> {
 
   @override
   void dispose() {
-    nameController.dispose();
+    firstNameController.dispose();
+    lastNameController.dispose();
     emailController.dispose();
+    phoneController.dispose();
+    addressController.dispose();
+    loginController.dispose();
     passwordController.dispose();
     super.dispose();
+  }
+
+  bool isEmailValid(String email) {
+    // Utilisation d'une expression régulière pour valider l'e-mail
+    final emailRegex = RegExp(r'^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$');
+    return emailRegex.hasMatch(email);
+  }
+
+  bool isPasswordValid(String password) {
+    // Utilisation d'une expression régulière pour valider le mot de passe
+    final passwordRegex =
+        RegExp(r'^(?=.*[A-Z])(?=.*\d)(?!.*[&%$#@!])[A-Za-z\d&%$#@!]{8,12}$');
+    return passwordRegex.hasMatch(password);
+  }
+
+  bool isPhoneValid(String phone) {
+    // Utilisation d'une expression régulière pour valider le numéro de téléphone
+    final phoneRegex =
+        RegExp(r'^\d{10}$'); // Valide un numéro de téléphone de 10 chiffres
+    return phoneRegex.hasMatch(phone);
   }
 
   @override
@@ -41,8 +73,13 @@ class _SignUpInputScreenState extends State<SignUpInputScreen> {
             children: [
               // Champ de texte pour le nom complet
               AppInputText(
-                controller: nameController,
-                hint: "FullName", // Invite de champ pour le nom complet
+                controller: firstNameController,
+                hint: "FirstName", // Invite de champ pour le nom complet
+              ),
+              SizedBox(height: 20),
+              AppInputText(
+                controller: lastNameController,
+                hint: "LastName", // Invite de champ pour le nom complet
               ),
               SizedBox(height: 20),
               // Champ de texte pour l'e-mail
@@ -50,6 +87,23 @@ class _SignUpInputScreenState extends State<SignUpInputScreen> {
                 controller: emailController,
                 hint: "Email", // Invite de champ pour l'e-mail
                 keyboardType: TextInputType.emailAddress,
+              ),
+              SizedBox(height: 20),
+              AppInputText(
+                controller: phoneController,
+                hint: "Téléphone",
+                keyboardType: TextInputType.phone,
+              ),
+              SizedBox(height: 20),
+              AppInputText(
+                controller: addressController,
+                hint: "Adresse",
+                keyboardType: TextInputType.emailAddress,
+              ),
+              SizedBox(height: 20),
+              AppInputText(
+                controller: loginController,
+                hint: "Login",
               ),
               SizedBox(height: 20),
               // Champ de texte pour le mot de passe
@@ -66,26 +120,93 @@ class _SignUpInputScreenState extends State<SignUpInputScreen> {
                 borderRadius: 30,
                 fontSize: 17,
                 fontWeight: FontWeight.w600,
-                onTap: () {
-                  // Créez une instance de User avec les données saisies
-                  final user = User(
-                    firstName: "", // À compléter avec le prénom
-                    lastName: "À compléter", // À compléter avec le nom de famille
-                    email: "", // À compléter avec l'e-mail
-                    address: "À compléter", // À compléter avec l'adresse
-                    phoneNumber: "123-456-7890", // À compléter avec le numéro de téléphone
-                    password: "", // À compléter avec le mot de passe
-                    photoUrl: "", // À compléter avec l'URL de la photo
-                  );
+                onTap: () async {
+                  final firstName = firstNameController.text;
+                  final lastName = lastNameController.text;
+                  final email = emailController.text;
+                  final address = addressController.text;
+                  final phone = phoneController.text;
+                  final login = loginController.text;
+                  final password = passwordController.text;
 
-                  // Passez l'instance de User à ProfileScreen
-                  Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => ProfileScreen(/*user: user*/), // Passez l'utilisateur à l'écran de profil
-                    ),
+                  if (!isEmailValid(email)) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text('L\'adresse e-mail n\'est pas valide.'),
+                        backgroundColor: Colors.red,
+                      ),
+                    );
+                    return;
+                  }
+                  if (!isPasswordValid(password)) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text('Le mot de passe n\'est pas valide.'),
+                        backgroundColor: Colors.red,
+                      ),
+                    );
+                    return;
+                  }
+
+                  if (!isPhoneValid(phone)) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content:
+                            Text('Le numéro de téléphone n\'est pas valide.'),
+                        backgroundColor: Colors.red,
+                      ),
+                    );
+                    return;
+                  }
+
+                  final user = User(
+                      firstName: firstNameController.text,
+                      lastName: lastNameController.text,
+                      email: emailController.text,
+                      address: addressController.text,
+                      phone: phoneController.text,
+                      login: loginController.text,
+                      password: passwordController
+                          .text,
+                      //photoUrl: "", // À compléter avec l'URL de la photo
+                      soldeCarteCrous: 0);
+                  final response = await http.post(
+                    Uri.parse('http://localhost:8080/DelivCROUS/users'),
+                    headers: <String, String>{
+                      'Content-Type': 'application/json; charset=UTF-8',
+                    },
+                    body: jsonEncode(
+                        user.toJson()), // Convertir l'utilisateur en JSON
                   );
-                },
+                  print(user.toJson());
+                  print(response.statusCode);
+                  // Passez l'instance de User à ProfileScreen
+                  if (response.statusCode == 201) {
+      // L'inscription a réussi, redirigez l'utilisateur vers le profil
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => LoginInputScreen(),
+        ),
+      );
+    } else if (response.statusCode == 409) {
+      // L'email existe déjà, affichez un message d'erreur
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('L\'email existe déjà dans la base de données.'),
+          backgroundColor: Colors.red,
+        ),
+      );
+    } else {
+      // Gestion d'autres erreurs
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Erreur lors de l\'inscription.'),
+          backgroundColor: Colors.red,
+        ),
+      );
+    }
+  },
                 text: "SignUp", // Texte du bouton d'inscription
                 textColor: athens_gray, // Couleur du texte du bouton
               ),
